9959c4bd9a5faadcc52b89ea7d60d6fd2f0049a6
Flávio Gomes <a62068@gmail.com>
2023-09-07 20:29:04 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let exp1 = document.getElementById("exp1");
+exp1.addEventListener('click', function(){
+    let desc1 = document.getElementById("desc1");
+    //desc1.setAttribute('style', 'display:none');
+    desc1.classList.toggle('esconder');
+})
+let exp2 = document.getElementById("exp2");
+exp2.addEventListener('click', function(){
+    let desc2 = document.getElementById("desc2");
+     desc2.classList.toggle('esconder');
+})
+let exp3 = document.getElementById("exp3");
+exp3.addEventListener('click', function(){
+    let desc3 = document.getElementById("desc3");
+     desc3.classList.toggle('esconder');
+})
